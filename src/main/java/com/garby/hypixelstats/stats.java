56f62562d77178242fda9259ee5583c963404554
Garbygas <75593400+Garbygas@users.noreply.github.com>
2022-01-26 19:59:43 -0600

    gui p1

--- src/main/java/com/garby/hypixelstats/stats.java
+++ src/main/java/com/garby/hypixelstats/stats.java
@@ -16,136 +16,9 @@ import java.util.Scanner;
 import java.util.UUID;
 
 public class stats {
-    public static void main(String[] args) {
+    public static Object[] main(String username, boolean debug) {
         try {
-            if (args.length == 0) {
-                final String path = stats.class.getProtectionDomain().getCodeSource().getLocation().getPath().replaceFirst("/", "");
-                Runtime.getRuntime().exec("cmd /c start cmd.exe /c \"java -jar " + path + " -w\"");
-
-            } else {
-                if (args[0].equals("-debug")) {
-                    System.out.println("Debug mode enabled");
-                    stats.run(args[1]);
-                } else {
-                    /* this fetches my api key from the file key.java
-                     it looks like this:
-
-                        package com.garby.hypixelstats;
-
-                            public class key {
-                                public static final String API_KEY = "<api key goes here>";
-                            }
-                     */
-                    stats.run(key.API_KEY);
-
-
-                }
-            }
-
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage() + "\n");
-            e.printStackTrace();
-        }
-    }
-
-
-    public static void run(String key) {
-        //to debug this, use -debug <api key>
-
-        try { //check if the api key is valid, if not, use catch statement and print error
-            HypixelAPI api = new HypixelAPI(UUID.fromString(key));
-            //asks the user for a username to get stats for
-            System.out.println("Type a username to view their bedwars stats, or type \"q\" to quit!");
-            Scanner sc = new Scanner(System.in);
-            while (sc.hasNextLine()) {
-
-                // Get the line of text that was typed
-                String line = sc.nextLine().replaceAll("\n", "");
-
-                // Make sure some text was typed
-                if (line.length() == 0) {
-                    continue;
-                }
-
-                // Quit if the input is "Q" or "q"
-                if (line.equalsIgnoreCase("Q")) {
-                    System.exit(1);
-                }
-
-            /*
-            Make sure the username is valid
-            The regular expression (RegEx) used here checks for between 3 and 16 characters that are
-            in the ranges A-Z, a-z, 0-9, or _ (underscores)
-             */
-                if (!line.matches("^\\w{3,16}$")) {
-                    System.err.println("Invalid username. Please try again!");
-                    continue;
-                }
-                //use mojang to get the uuid of the username
-                URL url = new URL("https://api.mojang.com/users/profiles/minecraft/" + line);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-                connection.setRequestMethod("GET");
-                connection.setRequestProperty("Content-Type", "application/json");
-                connection.setRequestProperty("Accept", "application/json");
-                InputStream responseStream = connection.getInputStream();
-                if (connection.getResponseCode() != 200) {
-                    System.err.println("Error. Please try again!");
-                    continue;
-                }
-                Reader responseStreamScanner = new InputStreamReader(responseStream);
-                JsonObject jsonObject = JsonParser.parseReader(responseStreamScanner).getAsJsonObject();
-                String uuid = jsonObject.get("id").getAsString();
-
-                //get the stats of the found uuid
-                api.getPlayerByUuid(uuid).whenComplete((response, error) -> {
-
-                    // Check if there was an API error
-                    if (error != null) {
-                        System.err.println("Error: " + error.getMessage());
-                        return;
-                    }
-
-                    // Get the player object from the response. This object stores any available information about a given player
-                    JsonObject player = response.getPlayer();
-
-                    if (player != null) {
-
-                    /*
-                    The player was found; print some basic info about them
-                    Not every player has all of these fields (for example, staff members don't have
-                    "mostRecentGameType"), so I added a handy little method to check if a field
-                    exists. If it doesn't, it returns "N/A" rather than throwing a
-                    NullPointerException
-                     */
-                        final double finalKills = Integer.parseInt(getFields(new String[]{"stats", "Bedwars", "final_kills_bedwars"}, player));
-                        final double finalDeaths = Integer.parseInt(getFields(new String[]{"stats", "Bedwars", "final_deaths_bedwars"}, player));
-                        final double fkdr = finalKills / finalDeaths;
-                        final double kills = Integer.parseInt(getFields(new String[]{"stats", "Bedwars", "kills_bedwars"}, player));
-                        final double deaths = Integer.parseInt(getFields(new String[]{"stats", "Bedwars", "deaths_bedwars"}, player));
-                        final double kdr = kills / deaths;
-                        final double wins = Integer.parseInt(getFields(new String[]{"stats", "Bedwars", "wins_bedwars"}, player));
-                        final double losses = Integer.parseInt(getFields(new String[]{"stats", "Bedwars", "losses_bedwars"}, player));
-                        final double wl = wins / losses;
-                        System.out.println("Username: " + getFields(new String[]{"displayname"}, player) + "\n" +
-                                "Bedwars Stats: \n" +
-                                "Level: " + getFields(new String[]{"achievements", "bedwars_level"}, player) + "\n" +
-                                "games played: " + fd(wins + losses, 0) + "\n" +
-                                "Beds Broken: " + fd(Integer.parseInt(getFields(new String[]{"stats", "Bedwars", "beds_broken_bedwars"}, player)), 0) + "\n" +
-                                "Wins/loss: " + fd(wins, 0) + "/" + fd(losses, 0) + " or " + fd(wl, 2) + "\n \n" +
-                                "Final Kills/deaths: " + fd(finalKills, 0) + "/" + fd(finalDeaths, 0) + " or " + fd(fkdr, 2) + "\n \n" +
-                                "Kills/Deaths: " + fd(kills, 0) + "/" + fd(deaths, 0) + " or " + fd(kdr, 2) + "\n" +
-                                "-----------------------------------------------------"
-                        );
-
-
-                    } else {
-
-                        // If we're here, it means that Hypixel has no info on this player
-                        System.err.println("That player was not found");
-                    }
-                });
-            }
+            return stats.lookup(username, debug);
         } catch (Exception e) {
             System.err.println("ERROR: " + e.getMessage() + "\n");
             e.printStackTrace();
@@ -154,6 +27,122 @@ public class stats {
                 System.exit(2);
             }
         }
+        return null;
+        //this fetches my api key from the file key.java
+        //it looks like this:
+             /*
+                package com.garby.hypixelstats;
+
+                public class key {
+                    public static final String API_KEY = "<api key goes here>";
+                }
+              */
+
+
+    }
+
+
+    public static Object[] lookup(String line, boolean debug) throws IOException {
+        String key = com.garby.hypixelstats.key.API_KEY;
+        //to debug this, use -debug api_key
+
+
+        HypixelAPI api = new HypixelAPI(UUID.fromString(key));
+        // Make sure some text was typed
+        if (line.length() == 0) {
+            System.out.println("No username entered");
+            return null;
+        }
+
+        // Quit if the input is "Q" or "q"
+        if (line.equalsIgnoreCase("Q")) {
+            System.exit(1);
+        }
+
+            /*
+            Make sure the username is valid
+            The regular expression (RegEx) used here checks for between 3 and 16 characters that are
+            in the ranges A-Z, a-z, 0-9, or _ (underscores)
+             */
+        if (!line.matches("^\\w{3,16}$")) {
+            System.err.println("Invalid username");
+            return null;
+        }
+        //use mojang to get the uuid of the username
+        URL url = new URL("https://api.mojang.com/users/profiles/minecraft/" + line);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("Accept", "application/json");
+        InputStream responseStream = connection.getInputStream();
+        if (connection.getResponseCode() != 200) {
+            System.err.println("Error. Please try again!");
+            return null;
+        }
+        Reader responseStreamScanner = new InputStreamReader(responseStream);
+        JsonObject jsonObject = JsonParser.parseReader(responseStreamScanner).getAsJsonObject();
+        String uuid = jsonObject.get("id").getAsString();
+
+        //get the stats of the found uuid
+        url = new URL("https://api.hypixel.net/player?key=" + key + "&uuid=" + uuid);
+        connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("Accept", "application/json");
+
+        responseStream = connection.getInputStream();
+        if (connection.getResponseCode() != 200) {
+            System.err.println("Error. Please try again!");
+            return null;
+        }
+        responseStreamScanner = new InputStreamReader(responseStream);
+        jsonObject = JsonParser.parseReader(responseStreamScanner).getAsJsonObject();
+        JsonObject player = jsonObject.get("player").getAsJsonObject();
+        JsonObject stats = player.get("stats").getAsJsonObject();
+
+
+        // Get the player object from the response. This object stores any available information about a given player
+
+        if (player != null) {
+
+                    /*
+                    The player was found; print some basic info about them
+                    Not every player has all of these fields (for example, staff members don't have
+                    "mostRecentGameType"), so I added a handy little method to check if a field
+                    exists. If it doesn't, it returns "N/A" rather than throwing a
+                    NullPointerException
+                     */
+            //^username ^level ^games played ^Wins ^Losses ^wlr ^fk ^fd ^fkdr ^k ^d  ^kdr
+            final double finalKills = Integer.parseInt(getFields(new String[]{"Bedwars", "final_kills_bedwars"}, stats));
+            final double finalDeaths = Integer.parseInt(getFields(new String[]{"Bedwars", "final_deaths_bedwars"}, stats));
+            final double fkdr = finalKills / finalDeaths;
+            final double kills = Integer.parseInt(getFields(new String[]{"Bedwars", "kills_bedwars"}, stats));
+            final double deaths = Integer.parseInt(getFields(new String[]{"Bedwars", "deaths_bedwars"}, stats));
+            final double kdr = kills / deaths;
+            final double wins = Integer.parseInt(getFields(new String[]{"Bedwars", "wins_bedwars"}, stats));
+            final double losses = Integer.parseInt(getFields(new String[]{"Bedwars", "losses_bedwars"}, stats));
+            final double wl = wins / losses;
+
+            System.out.println(
+                    "\nUsername: " + getFields(new String[]{"displayname"}, player) + "\n" +
+                            "Bedwars Stats: \n" +
+                            "Level: " + getFields(new String[]{"achievements", "bedwars_level"}, player) + "\n" +
+                            "games played: " + fd(wins + losses, 0) + "\n" +
+                            "Beds Broken: " + fd(Integer.parseInt(getFields(new String[]{"Bedwars", "beds_broken_bedwars"}, stats)), 0) + "\n" +
+                            "Wins/loss: " + fd(wins, 0) + "/" + fd(losses, 0) + " or " + fd(wl, 2) + "\n \n" +
+                            "Final Kills/deaths: " + fd(finalKills, 0) + "/" + fd(finalDeaths, 0) + " or " + fd(fkdr, 2) + "\n \n" +
+                            "Kills/Deaths: " + fd(kills, 0) + "/" + fd(deaths, 0) + " or " + fd(kdr, 2) + "\n" +
+                            "-----------------------------------------------------"
+            );
+            return new Object[]{getFields(new String[]{"displayname"}, player),
+                    getFields(new String[]{"achievements", "bedwars_level"}, player),
+                    fd(wins + losses, 0),
+                    fd(Integer.parseInt(getFields(new String[]{"Bedwars", "beds_broken_bedwars"}, stats)), 0),
+                    fd(wins, 0), fd(losses, 0), fd(wl, 2), fd(finalKills, 0), fd(finalDeaths, 0),
+                    fd(fkdr, 2), fd(kills, 0), fd(deaths, 0), fd(kdr, 2)};
+        }
+        return new Object[]{"E", "R", "R", "O", "R"};
     }
 
     private static String fd(double d, int i) {
